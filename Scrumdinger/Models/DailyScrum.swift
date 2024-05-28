a8c505f40a07e42fb23e419e861cd5f833181cf2
joy jain <joyshah204@gmail.com>
2024-05-29 00:38:19 +0530

    DailyScrum file create

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,8 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by JOY JAIN on 29/05/24.
+//
+
+import Foundation
--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -26,4 +26,15 @@ enum Theme: String {
     case tan
     case teal
     case yellow
+    
+    var accentColor: Color {
+        switch self {
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan,
+                .teal, .yellow: return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        }
+    }
+    var mainColor: Color {
+        Color(rawValue)
+    }
 }
