db5f2411047e64e791b729baf8a4df669c4fdd5d
joy jain <joyshah204@gmail.com>
2024-05-29 00:28:48 +0530

    assets add

---
+++ Scrumdinger/Models/Theme.swift
@@ -0,0 +1,29 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by JOY JAIN on 29/05/24.
+//
+
+//import Foundation
+import SwiftUI
+// SwiftUI treats colors as View instances that you can add directly to a view hierarchy.
+
+enum Theme: String {
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+}
