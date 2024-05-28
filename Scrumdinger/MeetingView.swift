17a5dfcccc8c03ecc9d2ab089bfe5f6fb6a15189
joy jain <joyshah204@gmail.com>
2024-05-28 18:25:55 +0530

    initiallisation contentView to meeting view

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by JOY JAIN on 28/05/24.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+    }
+}
+
+//#Preview {
+//    ContentView()
+//}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
