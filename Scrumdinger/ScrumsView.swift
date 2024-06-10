d6907282761cf7afa9a7bddf1335293730b1a215
joy jain <joyshah204@gmail.com>
2024-06-10 20:55:16 +0530

    Display a list of daily scrums

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,29 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by JOY JAIN on 10/06/24.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+//#Preview {
+//    ScrumsView()
+//}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
